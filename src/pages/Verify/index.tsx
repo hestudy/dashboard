d107882631e702b2f72242bf06e73c6dca807b35
HeStudy <1467602180@qq.com>
2024-07-11 19:23:18 +0800

    🎨 feat: 优化验证页面逻辑 在验证页面中，引入了useUser钩子和useNavigate钩子，优化了用户验证逻辑，并在用户验证成功后自动重定向到首页。

--- src/pages/Verify/index.tsx
+++ src/pages/Verify/index.tsx
@@ -1,27 +1,39 @@
 import FullCenter from "@/components/FullCenter";
 import FullLoading from "@/components/FullLoading";
+import { useUser } from "@/hooks/useUser";
 import { supabase } from "@/lib/supabase";
 import { useRequest } from "ahooks";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Verify = () => {
+  const navigate = useNavigate();
+  const { user, runAsync } = useUser();
+
   const { loading } = useRequest(async () => {
     if (location.hash) {
       await supabase.getInstance().auth.verifyOtp({
         token_hash: location.hash,
         type: "email",
-        options: {
-          redirectTo: import.meta.env.VITE_SITE_URL,
-        },
       });
+      await runAsync(true);
     }
   });
 
-  if (!location.hash) {
-    return <FullCenter>Invalid verification link</FullCenter>;
-  }
+  useEffect(() => {
+    if (user?.id) {
+      navigate("/", {
+        replace: true,
+      });
+    }
+  }, [user]);
 
   if (loading) {
     return <FullLoading></FullLoading>;
+  }
+
+  if (!location.hash) {
+    return <FullCenter>Invalid verification link</FullCenter>;
   }
 
   return <></>;
